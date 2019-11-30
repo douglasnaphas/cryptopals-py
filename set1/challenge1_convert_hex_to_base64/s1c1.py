2f4680bc51a5509e48f5fddf999141381e969e4f
Douglas Naphas <douglasnaphas@gmail.com>
2019-11-30 02:03:56 -0500

    Solve set 1 challenge 1 hex to b64

--- set1/challenge1_convert_hex_to_base64/s1c1.py
+++ set1/challenge1_convert_hex_to_base64/s1c1.py
@@ -1,3 +1,6 @@
+import math
+import base64
+
 class S1C1():
     @staticmethod
     def ret_true():
@@ -5,4 +8,8 @@ class S1C1():
 
     @staticmethod
     def hex_to_base64(hx):
-        return "x"
+        hx_str = '0x' + hx
+        n = int(hx_str, 16)
+        num_bytes = math.ceil(n.bit_length() / 8)
+        b = (n).to_bytes(num_bytes, 'big')
+        return str(base64.b64encode(b))[2:-1]
