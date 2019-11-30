95ad5dacd1d3ed14db4979d8837146fccde40876
Douglas Naphas <douglasnaphas@gmail.com>
2019-11-29 23:13:59 -0500

    Use parameterized in a test

--- set1/challenge1_convert_hex_to_base64/test/test_s1c1.py
+++ set1/challenge1_convert_hex_to_base64/test/test_s1c1.py
@@ -1,5 +1,6 @@
 import unittest
 from set1.challenge1_convert_hex_to_base64.s1c1 import S1C1
+from parameterized import parameterized
 
 class TestCanary(unittest.TestCase):
     def test_succeed(self):
@@ -8,3 +9,9 @@ class TestCanary(unittest.TestCase):
     def test_import(self):
         self.assertTrue(S1C1.ret_true())
 
+    @parameterized.expand([
+        (2, 2, 4),
+        (1, 1, 2),
+    ])
+    def test_p(self, a, b, expected):
+        self.assertEqual(a + b, expected)
