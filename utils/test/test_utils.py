7df12820c938c8b41f7d79845d7b517cd426df1c
Douglas Naphas <douglasnaphas@gmail.com>
2019-12-25 12:34:14 -0500

    Set up for s1c3

--- utils/test/test_utils.py
+++ utils/test/test_utils.py
@@ -9,3 +9,8 @@ class TestNby(unittest.TestCase):
     ])
     def test_nby(self, n, expected):
         self.assertEqual(Utils.nby(n), expected)
+
+class TestScoreEnglish(unittest.TestCase):
+    @parameterized.expand([('a', 0)])
+    def test_score_english(self, s, expected):
+        self.assertEqual(Utils.score_english(s), expected)
--- utils/utils.py
+++ utils/utils.py
@@ -14,3 +14,17 @@ class Utils():
         if n == 0:
             return 1
         return math.ceil(n.bit_length() / 8)
+
+    @staticmethod
+    def score_english(txt):
+        """Return the Euclidean distance between txt and a vector representing
+        the frequency of characters in English.
+
+        Positional argument:
+        txt -- The string to score.
+        """
+        return 0
+
+    # something based on
+    # functools.reduce(lambda beg, inc : beg + ord(inc), 'abcdefghijklmnopqrstuvwxyz', 0)
+    # and ascii_lowercase
