edb3f65215bab711eee43cb736c47c35d13341a4
Douglas Naphas <douglasnaphas@gmail.com>
2020-07-03 10:40:34 -0700

    Refactor s1c3 to use repeated_XOR
    https://cryptopals.com/sets/1/challenges/3

--- set1/challenge3_1_byte_XOR_cipher/s1c3.py
+++ set1/challenge3_1_byte_XOR_cipher/s1c3.py
@@ -46,39 +46,19 @@ class S1C3():
         """ 
 
 def main():
-    # we will use S1C2.fixed_XOR(hex string, hex string)
-
     # get the input string as a number
     ctext = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-    ctn = Utils.hexstr2num(ctext)
-    
-    # get the length of the input
-    ctlen = int(len(ctext) / 2) # / 2 because every 2 hexes is 1 8-bit char
 
     # get the set of possible key characters (one character each)
     FIRST_PRINTABLE_DEC = 32
     LAST_PRINTABLE_DEC = 126
     candidate_chars = set(range(FIRST_PRINTABLE_DEC, LAST_PRINTABLE_DEC + 1))
 
-    # for each candidate char, make what the key would be by reproducing it
-    # ctlen times, and save its score in a dictionary keyed on the key char
-    d = {cc : Utils.score_english(S1C2.fixed_XOR(ctext, hex(ord(chr(cc)))[2:])) for cc in candidate_chars}
-#    print(d)
-#    print(e)
-    # f = {cc : hex(ord(chr(cc)))[2:] * ctlen for cc in candidate_chars}
-    # for k, v in f.items():
-    #     print(k, ' : ', v)
-    e = {cc : S1C2.fixed_XOR(ctext, hex(ord(chr(cc)))[2:] * ctlen) for cc in candidate_chars}
-    # e = {cc : Utils.repeated_XOR(ctext, chr(cc)) for cc in candidate_chars}
-    # for k, v in e.items():
-    #     print(k, ' : ', v)
-    g = {k : bytes.fromhex(v).decode('utf-8') for k, v in e.items()}
-    # exclude anything in the 
-    print("new approach...")
+    e = {cc : Utils.repeated_XOR(ctext, chr(cc)) for cc in candidate_chars}
     loscore = 100
     lokey = 'nothing'
     loval = 'nothing'
-    for k, v in g.items():
+    for k, v in e.items():
         try:
             score = Utils.score_english(v)
             if score < 0.24:
@@ -90,15 +70,6 @@ def main():
         except:
             pass
     print(lokey, " : ", loscore, " : ", loval)
-
-#    h = {k : Utils.score_english(v) for k, v in g.items()}
-#    for k, v in h.items():
-#        print(k, ' : ', v)
-    
-    
-
-    
-
 
 
 if __name__ == "__main__":
