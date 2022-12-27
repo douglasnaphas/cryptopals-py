051b23cb73ed60c9f3f6c1db57306cf15ddcdad6
Douglas Naphas <douglasnaphas@gmail.com>
2022-12-27 08:19:46 -0500

    Read in the ciphertext
    gh-5

--- set1/challenge7_aes_ecb/test/test_s1c7.py
+++ set1/challenge7_aes_ecb/test/test_s1c7.py
@@ -4,9 +4,12 @@ from set1.challenge7_aes_ecb.s1c7 import S1C7
 
 class TestDecryptingFiles(unittest.TestCase):
     @parameterized.expand([
-        ('7.txt', '7.decrypted.txt')
+        ('set1/challenge7_aes_ecb/test/7.txt', '7.decrypted.txt')
     ])
     def test_decrypting_files(self, infile, outfile):
         self.assertEqual('a', 'a')
+        f = open(infile, 'r')
+        ciphertext = f.read()
+        f.close()
 
 
