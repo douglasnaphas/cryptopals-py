2ec76b0ae8932e13e5e47586e978edaf705cce24
Douglas Naphas <douglasnaphas@gmail.com>
2022-12-27 08:22:28 -0500

    Format s1c7 test

--- set1/challenge7_aes_ecb/test/test_s1c7.py
+++ set1/challenge7_aes_ecb/test/test_s1c7.py
@@ -2,14 +2,14 @@ import unittest
 from parameterized import parameterized
 from set1.challenge7_aes_ecb.s1c7 import S1C7
 
+
 class TestDecryptingFiles(unittest.TestCase):
     @parameterized.expand([
-        ('set1/challenge7_aes_ecb/test/7.txt', '7.decrypted.txt')
+        ('set1/challenge7_aes_ecb/test/7.txt',
+         'set1/challenge7_aes_ecb/test/7.decrypted.txt')
     ])
     def test_decrypting_files(self, infile, outfile):
         self.assertEqual('a', 'a')
         f = open(infile, 'r')
         ciphertext = f.read()
         f.close()
-
-
