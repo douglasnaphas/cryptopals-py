451498f14f46beee3bbb6e51b2a509c5e0e71f4e
Douglas Naphas <douglasnaphas@gmail.com>
2022-12-30 19:50:57 -0500

    Use the Python crypto library, lost in types
    gh-5
    
    This seems to be doing something, but I am so lost between string types
    (i.e., bytes, byte arrays, UTF-8, etc), that I can't tell if the result
    I'm obtaining is right.

--- set1/challenge7_aes_ecb/test/test_s1c7.py
+++ set1/challenge7_aes_ecb/test/test_s1c7.py
@@ -1,6 +1,7 @@
 import unittest
 from parameterized import parameterized
 from set1.challenge7_aes_ecb.s1c7 import S1C7
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 class TestDecryptingFiles(unittest.TestCase):
@@ -17,3 +18,12 @@ class TestDecryptingFiles(unittest.TestCase):
         expected = exfile.read()
         exfile.close()
         self.assertEqual(S1C7.s1c7(ciphertext), expected)
+        key = bytes.fromhex('YELLOW SUBMARINE'.encode('utf-8').hex())
+        cipher = Cipher(algorithms.AES(key), modes.ECB())
+        encryptor = cipher.encryptor()
+        ct = encryptor.update(ciphertext.encode("utf-8")) + encryptor.finalize()
+        decryptor = cipher.decryptor()
+        result = decryptor.update(ct) + decryptor.finalize()
+        print(ciphertext)
+        print(ct)
+        print(result)
