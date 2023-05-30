3ef527d9f05ed37c7297237703e68b35d95e5f10
Douglas Naphas <douglasnaphas@gmail.com>
2023-05-30 00:27:11 -0400

    Complete set2 with failing test not picked up!
    gh-13
    
    I have made a test fail in set2, but my test file is not being run

---
+++ set2/challenge9_pkcs7_padding/test_s2c9.py
@@ -0,0 +1,14 @@
+import unittest
+from parameterized import parameterized
+from utils.utils import Utils
+
+
+class TestS2C9(unittest.TestCase):
+    @parameterized.expand(
+        [
+            ("YELLOW SUBMARINEs", 20, "YELLOW SUBMARINE\x04\x04\x04\x04"),
+            ("YELLOW SUBMARINE", 17, "YELLOW SUBMARINE\x04")
+        ]
+    )
+    def test_pad2(self, s, l, expected):
+        self.assertEqual(Utils.pad(s, l), expected)
